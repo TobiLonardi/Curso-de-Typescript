622489013cae2cb0a53b5a4ce267b0baa3357b42
Tobias Lonardi <tobilonardi@gmail.com>
2025-10-29 18:24:14 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const msg = "Hola Mundo";
+const hero = {
+    name: "Ironman",
+    age: 45
+};
+hero.age = 50;
+console.log(msg);
+console.log(hero.age + 1);
+//# sourceMappingURL=app.js.map
