599852ebfb7cedf6b298a8b46422f3c503d124e5
Tobias Lonardi <tobilonardi@gmail.com>
2025-11-05 19:26:26 -0300

    app modificado para prueba

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 "use strict";
 (function () {
-    var a = 10;
-    console.log(a);
-});
+    // Tipos
+    var batman = 'Bruce';
+    var superman = 'Clark';
+    var existe = false;
+    // Tuplas
+    var parejaHeroes = [batman, superman];
+    var villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    var Fuerza;
+    (function (Fuerza) {
+        Fuerza[Fuerza["fuerzaAcuaman"] = 0] = "fuerzaAcuaman";
+        Fuerza[Fuerza["fuerzaBatman"] = 1] = "fuerzaBatman";
+        Fuerza[Fuerza["fuerzaFlash"] = 5] = "fuerzaFlash";
+        Fuerza[Fuerza["fuerzaSuperman"] = 100] = "fuerzaSuperman";
+    })(Fuerza || (Fuerza = {}));
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+        return;
+    }
+    // Aserciones de Tipo
+    var poder = '100';
+    var largoDelPoder = poder.length;
+    console.log(largoDelPoder);
+})();
--- app.ts
+++ app.ts
@@ -1,4 +1,42 @@
-(()=>{
-    const a:number = 10;
-    console.log(a);
-})
+(() => {
+
+    // Tipos
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
+  
+    const existe:boolean = false;
+  
+    // Tuplas
+    const parejaHeroes:[string,string] = [batman,superman];
+    const villano: [string,number,boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+    enum Fuerza {
+      fuerzaAcuaman= 0,
+      fuerzaBatman= 1,
+      fuerzaFlash= 5,
+      fuerzaSuperman= 100
+    }
+  
+    // Retorno de funciones
+    function activar_batiseñal():string{
+      return 'activada';
+    }
+  
+    function pedir_ayuda():void{
+      console.log('Auxilio!!!');
+      return;
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
